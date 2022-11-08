4d791290c6e9b71a6096dd1de70c024644d6abf1
팔만대장경 <honggildong12@nate.co.kr>
2022-11-08 17:40:56 +0900

    samsung first commit

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
