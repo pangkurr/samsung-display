a40b20d1cb245f4a1e41582eabf1adca83e5eaa8
pangkurr <dbswjdgy1070@gmail.com>
2022-11-15 01:55:44 +0900

    section css

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,5 @@
 var swiper = new Swiper(".mySwiper1", {
+  speed:2000,
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -13,6 +14,7 @@ var swiper = new Swiper(".mySwiper1", {
 });
 
 var swiper = new Swiper(".mySwiper2", {
+  speed:2000,
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -20,6 +22,16 @@ var swiper = new Swiper(".mySwiper2", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
