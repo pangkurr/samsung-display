32f9876ab0e3d8ad11aa7c2dc3b127374a67718f
JH_yo <dbswjdgy1070@gmail.com>
2022-11-15 17:41:14 +0900

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,30 +1,38 @@
 var swiper = new Swiper(".mySwiper1", {
-  speed:2000,
+  speed:1500,
   slidesPerView: 1,
-  spaceBetween: 30,
   loop: true,
+  touchRatio:0,
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction: false
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper_box .swiper-button-next",
+    prevEl: ".swiper_box .swiper-button-prev",
   },
 });
 
 var swiper = new Swiper(".mySwiper2", {
-  speed:2000,
+  speed:1500,
   slidesPerView: 1,
-  spaceBetween: 30,
   loop: true,
+  touchRatio:0,
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction: false
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper_box .swiper-button-next",
+    prevEl: ".swiper_box .swiper-button-prev",
   },
 });
 
@@ -33,7 +41,17 @@ var swiper = new Swiper(".mySwiper3", {
   spaceBetween: 30,
   slidesPerGroup: 1,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".p_wrap .swiper-button-next",
+    prevEl: ".p_wrap .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper4", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: ".s_wrap .swiper-button-next",
+    prevEl: ".s_wrap .swiper-button-prev",
   },
 });
